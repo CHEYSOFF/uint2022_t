24031248b9176bce0f0c3eb7c54a2eeb85afdabb
CHEYSOFF <slobberyblanket@gmail.com>
2022-10-01 23:03:18 +0300

    Revert "implemented some functions"
    This reverts commit 3d577b97d857bf3fe4c0c394b7f14af46ca8071b.

--- lib/number.h
+++ lib/number.h
@@ -2,34 +2,27 @@
 #include <cinttypes>
 #include <iostream>
 
-struct uint2022_t
-{
-    uint32_t num[64];
-    const size_t size = 64;
-    const size_t radix = (1ll << 32) - 1;
-    uint2022_t()
-    {
-        for (int i = 0; i < 64; i++)
-            num[i] = 0;
-    }
+
+struct uint2022_t {
+    // implement
 };
 
 static_assert(sizeof(uint2022_t) <= 300, "Size of uint2022_t must be no higher than 300 bytes");
 
 uint2022_t from_uint(uint32_t i);
 
-uint2022_t from_string(const char *buff);
+uint2022_t from_string(const char* buff);
 
-uint2022_t operator+(const uint2022_t &lhs, const uint2022_t &rhs);
+uint2022_t operator+(const uint2022_t& lhs, const uint2022_t& rhs);
 
-uint2022_t operator-(const uint2022_t &lhs, const uint2022_t &rhs);
+uint2022_t operator-(const uint2022_t& lhs, const uint2022_t& rhs);
 
-uint2022_t operator*(const uint2022_t &lhs, const uint2022_t &rhs);
+uint2022_t operator*(const uint2022_t& lhs, const uint2022_t& rhs);
 
-uint2022_t operator/(const uint2022_t &lhs, const uint2022_t &rhs);
+uint2022_t operator/(const uint2022_t& lhs, const uint2022_t& rhs);
 
-bool operator==(const uint2022_t &lhs, const uint2022_t &rhs);
+bool operator==(const uint2022_t& lhs, const uint2022_t& rhs);
 
-bool operator!=(const uint2022_t &lhs, const uint2022_t &rhs);
+bool operator!=(const uint2022_t& lhs, const uint2022_t& rhs);
 
-std::ostream &operator<<(std::ostream &stream, const uint2022_t &value);
+std::ostream& operator<<(std::ostream& stream, const uint2022_t& value);
